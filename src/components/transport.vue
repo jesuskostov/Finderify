cf6b8916af8eebf7d7eb92630bffee5570068485
Isus Kostov <jesuskostov@gmail.com>
2020-10-15 10:58:32 +0300

    update

---
+++ src/components/transport.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Transportt</h1>
+        <p v-for="(transport, i) in transports" :key="i" @click="getSelected(transport.category)">{{transport.name}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'transport',
+    data() {
+        return {
+            choice: '',
+            transports: [
+                { name: 'ЖП - Гара', category: 'train_station' },
+                { name: 'Метро', category: 'subway_station' }, 
+                { name: 'Летище', category: 'airport' }, 
+                { name: 'Автогара', category: 'bus_station' }
+            ],
+        }
+    },
+    methods: {
+        getSelected(category) {
+            this.$emit('getSelected', category);
+        }
+    }
+}
+</script>
